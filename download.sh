ab6e3584cc5ab06e880e395277b257530e8c6160
FreezeFish <zzh1412@163.com>
2016-06-29 18:25:28 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,18 @@
+#This file is used to download the benchmark data
+mkdir benchmark
+cd benchmark
+
+
+
+#download the related files
+mkdir small_data
+cd small_data
+wget http://www.cbrc.kaust.edu.sa/readscan/download/simulation.fastq.gz
+gunzip simulation.fastq.gz
+cd ..
+
+mkdir middle_data
+cd middle_data
+wget http://**
+tar **
+cd ..
