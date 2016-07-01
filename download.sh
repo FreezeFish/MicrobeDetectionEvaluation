ab88eb41b9871ed46f77a79084902bfae2db9c5b
FreezeFish <zzh1412@163.com>
2016-07-01 15:13:40 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,8 +2,6 @@
 mkdir benchmark
 cd benchmark
 
-
-
 #download the related files
 mkdir small_data
 cd small_data
@@ -13,13 +11,13 @@ cd ..
 
 mkdir middle_data
 cd middle_data
-wget http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?cmd=dload&run_list=ERR205979&format=fastq
+wget http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?cmd=dload&run_list=SRR453448&format=fastq
 gunzip sra_data.fastq.gz
 cd ..
 
 mkdir large_data
 cd large_data
-wget http://
-gunzip
+wget http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?cmd=dload&run_list=ERR205979&format=fastq
+gunzip gunzip sra_data.fastq.gz
 cd ..
 
