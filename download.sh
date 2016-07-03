2a8ffdf0e970917c2f44706160b45a67017f09ad
FreezeFish <zzh1412@163.com>
2016-07-03 10:41:04 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,17 +7,20 @@ mkdir small_data
 cd small_data
 wget http://www.cbrc.kaust.edu.sa/readscan/download/simulation.fastq.gz
 gunzip simulation.fastq.gz
+mv simulation.fastq small_data.fastq
 cd ..
 
 mkdir middle_data
 cd middle_data
 wget http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?cmd=dload&run_list=SRR453448&format=fastq
 gunzip sra_data.fastq.gz
+mv sra_data.fastq middle_data.fastq
 cd ..
 
 mkdir large_data
 cd large_data
 wget http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?cmd=dload&run_list=ERR205979&format=fastq
 gunzip gunzip sra_data.fastq.gz
+mv sra_data.fastq large_data.fastq
 cd ..
 
