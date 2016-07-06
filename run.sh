25af600a1673ddcee0e3836e0f331ed787d8fce0
FreezeFish <zzh1412@163.com>
2016-07-06 17:43:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,9 @@
 #change the fa files to detect other calculation example
 echo "Using kraken to verify HiSeq_timing.fa"
 cd kraken/kraken-0.10.5-beta/kraken-software
-cat a.sh << EOF
-./kraken --db ../minikraken_20141208 ../HiSeq_timing.fa 
-EOF
+echo "./kraken --db ../minikraken_20141208 ../HiSeq_timing.fa" > a.sh
 chmod +x a.sh
-./a.sh  2>&1 | tee mylog.log
+/usr/bin/time -a -o time.log ./a.sh  2>&1 | tee result.log
 cd ../../../
 
 #testing readscan
@@ -20,19 +18,20 @@ readscan_makeflow.pl index -k 13 -s 13 human_genome.fasta
 readscan_makeflow.pl index -k 13 -s 6 microbes.fasta
 readscan_makeflow.pl search -h human_genome -p microbes -r results -T taxon simulation.fastq
 cd results
-make -j2
+echo "make -j2" > a.sh
+chmod +x a.sh
+/usr/bin/time -a -o time.log ./a.sh 2>&1 | tee result.log
 cd ../../
 
 #testing cs-score
 #change the fasta files to detect other calculation example
 echo "Using cs-score to detect test.fasta"
 cd cs-score/CSCORE_DISTRIBUTION
-cat a.sh << EOF
-/usr/bin/time -o logfile ./cscore ./test.fasta
-grep -o INVALID test.fasta.assign | wc -l 
-EOF
+echo "./cscore ./test.fasta" > a.sh
+echo "grep -o INVALID test.fasta.assign | wc -l " >> a.sh
+echo "wc -l" >> a.sh
 chmod +x a.sh
-./a.sh  2>&1 | tee mylog.log
+/usr/bin/time -a -o time.log ./a.sh 2>&1 | tee result.log
 cd ../../
 
 
