990080a2139ccbdde1a06ad94b585053d67442ab
FreezeFish <zzh1412@163.com>
2016-07-07 11:17:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,14 @@
 #change the fa files to detect other calculation example
 echo "Using kraken to verify HiSeq_timing.fa"
 cd kraken/kraken-0.10.5-beta/kraken-software
-echo "./kraken --db ../minikraken_20141208 ../HiSeq_timing.fa" > a.sh
-chmod +x a.sh
-/usr/bin/time -a -o time.log ./a.sh  2>&1 | tee result.log
+/usr/bin/time -a -o time.log ./kraken --db ../minikraken_20141208 ../HiSeq_timing.fa  2>&1 | tee result.log
+tac result.log | sed -n 2p >log
+echo -e "The classified sequences occupy \c"
+cat log | awk -F"[()]" '{print $2, ORS=""}'
+echo " of total sequences."
+echo -e "The runnig duration of the kraken is \c"
+head -n 1 time.log | awk -F "[user]" '{print $1, ORS=""}'
+echo "seconds."
 cd ../../../
 
 #testing readscan
@@ -17,10 +22,19 @@ cd readscan
 readscan_makeflow.pl index -k 13 -s 13 human_genome.fasta
 readscan_makeflow.pl index -k 13 -s 6 microbes.fasta
 readscan_makeflow.pl search -h human_genome -p microbes -r results -T taxon simulation.fastq
-cd results
-echo "make -j2" > a.sh
-chmod +x a.sh
-/usr/bin/time -a -o time.log ./a.sh 2>&1 | tee result.log
+cd results/simulation
+/usr/bin/time -a -o time.log make -j2 2>&1 | tee result.log
+cd output
+echo "print (100-" > a.py
+nl venn_stats.txt | sed -n '10p' | awk '{print $4, ORS=""}' >> a.py
+echo ")" >> a.py
+python a.py > log
+echo -e "The classified sequences occupy \c"
+cat log | awk '{print $0, ORS=""}'
+echo " of total sequences."
+echo -e "The runnig duration of the readscan is \c"
+head -n 1 time.log | awk -F "[user]" '{print $1}'
+echo "seconds."
 cd ../../
 
 #testing cs-score
@@ -28,8 +42,13 @@ cd ../../
 echo "Using cs-score to detect test.fasta"
 cd cs-score/CSCORE_DISTRIBUTION
 echo "./cscore ./test.fasta" > a.sh
+echo "INVALID: "
 echo "grep -o INVALID test.fasta.assign | wc -l " >> a.sh
-echo "wc -l" >> a.sh
+echo "HUMAN: "
+echo "grep -o HUMAN test.fasta.assign | wc -l "
+echo "PROKARYOTE: "
+echo "grep -o PROKARYOTE test.fasta.assign | wc -l "
+echo "wc -l test.fasta.assign" >> a.sh
 chmod +x a.sh
 /usr/bin/time -a -o time.log ./a.sh 2>&1 | tee result.log
 cd ../../
